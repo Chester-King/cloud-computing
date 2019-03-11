77dd96e6f7949cf81862952f89c484015f8f055f
Karen Pinto <karenpinto1602@gmail.com>
2019-03-11 09:38:53 +0530

    Update as of 9:40

--- App/app/src/main/java/com/example/amazonians/activities/LoginActivity.java
+++ App/app/src/main/java/com/example/amazonians/activities/LoginActivity.java
@@ -33,5 +33,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        (findViewById(R.id.textView4)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+
     }
 }
